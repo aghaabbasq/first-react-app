6bdef0961edceed20be896080dad32642b21e081
aghaabbasq <abbasagha201@gmail.com>
2018-05-11 14:10:14 +1000

    adding increment and decrement on click

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: true
+    };
+  }
+  toggle = () => {
+    this.setState({isOn: !this.state.isOn})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My First React App</h1>
-          <MyComponent name="Thinkmillers" />
-        </header>
+          <h2>Welcome to My first React</h2>
+          <MyComponent name="Thinkmillers 😃" />
+        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
         </p>
+        <button onClick={this.toggle}>
+          {this.state.isOn ? 'On' : 'Off'}
+        </button>
       </div>
     );
   }
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MyComponent = (props) => (
-  <div>Hello {props.name}</div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
 
-export default MyComponent;
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.IncrementItem}>Click to increment by 1</button>
+        <button onClick={this.DecreaseItem}>Click to decrease by 1</button>
+        <button onClick={this.ToggleClick}>
+          { this.state.show ? 'Hide number' : 'Show number' }
+        </button>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
+      </div>
+    );
+  }
+}
+
+export default App;
